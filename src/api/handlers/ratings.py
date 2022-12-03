d92129b90fde98b575e9a31dbf0e88d252406a96
thenesterov <thenesterov@proton.me>
2022-12-03 19:46:21 +0500

    Create ratings

--- src/api/handlers/ratings.py
+++ src/api/handlers/ratings.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from src.db.repositories.ratings import RatingsOperation
 
 router = APIRouter()
 
 
 @router.get('/get_ratings')
-def get_ratings():
-    pass
+def get_ratings(ratings_operation: RatingsOperation = Depends()):
+    return ratings_operation.get_rating()
--- src/db/repositories/ratings.py
+++ src/db/repositories/ratings.py
@@ -2,6 +2,7 @@ from fastapi import Depends
 
 from src.core.db import Session
 from src.db.common import get_database
+from src.db.models.queries import Query, Score
 from src.db.models.users import User
 
 
@@ -11,4 +12,23 @@ class RatingsOperation:
 
     def get_user_scores_by_id(self, id):
         user = self.session.query(User).filter(User.id == id).one()
+        query = self.session.query(Query).filter(Query.email == user.email).one()
 
+        scores = self.session.query(Score).filter(Score.id_query == query.id).all()
+
+        summation = 0
+
+        for score in scores:
+            summation += score.sum_score
+
+        return summation
+
+    def get_rating(self):
+        users = self.session.query(User).all()
+
+        rating = []
+
+        for user in users:
+            rating.append({'email': user.email, 'score': self.get_user_scores_by_id(user.id)})
+
+        return sorted(rating, key=lambda item: item['score'], reverse=True)

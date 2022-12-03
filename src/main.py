edcec355a2a706e9d910acab74eb71967b70ce38
thenesterov <thenesterov@proton.me>
2022-12-03 12:04:32 +0500

    Add CORS

--- src/main.py
+++ src/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.routes import router
 from src.core import authjwt
 from src.db import common
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # TODO: переместить в src/api/handlers/users

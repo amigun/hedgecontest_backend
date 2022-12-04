2857d85acce7f7c8c014b656712f5e04c6e83e21
thenesterov <thenesterov@proton.me>
2022-12-04 15:24:44 +0500

    Hot fix

--- src/api/dependencies/users.py
+++ src/api/dependencies/users.py
@@ -6,15 +6,11 @@ from src.db.repositories.users import UsersOperation
 class Need:
     def need(self, roles: list, token: dict):
         try:
-            print(f'\n\n\n{token}\n\n\n')
-            print(f'\n\n\n{roles}\n\n\n')
             role = token['sub'].split(':')[1]
         except TypeError:
-            print('typeerroooooooooooooooooor')
             return None
 
         if role in roles:
-            pass
+            return 'ok'
         else:
-            print('eeeeeeeeeeeeelse')
             return None
